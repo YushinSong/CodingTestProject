5da6a9f7a558617eba8fbd3470321e388c4aef3e
YushinSong <dbtls6513@naver.com>
2024-08-15 23:03:50 +0900

    [송유신] 물고기 종류 별 대어 찾기 문제

---
+++ CodingTestSolutions/src/SQL/ProgrammersFindingBigFishByFishType.java
@@ -0,0 +1,13 @@
+package src.SQL;
+
+public class ProgrammersFindingBigFishByFishType {
+//    -- 코드를 입력하세요
+//    SELECT f.ID , FISH_NAME, f.LENGTH FROM FISH_INFO f JOIN FISH_NAME_INFO fn
+//    ON f.FISH_TYPE = fn.FISH_TYPE
+//    WHERE f.FISH_TYPE IN (
+//            SELECT FISH_TYPE FROM FISH_INFO
+//            GROUP BY FISH_TYPE
+//            HAVING LENGTH = MAX(LENGTH)
+//    )
+//    ORDER BY ID;
+}
