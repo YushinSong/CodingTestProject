0e6ed3626464253ae32bec43a8922b6210f80264
dbtls6513 <dbtls6513@naver.com>
2024-08-20 08:51:59 +0900

    [송유신] 프로그래머스 수박수박수박수박수박수? 문제

---
+++ CodingTestSolutions/src/string/ProgrammersWatermelonWatermelonWatermelon.java
@@ -0,0 +1,9 @@
+package src.string;
+
+public class ProgrammersWatermelonWatermelonWatermelon {
+    class Solution {
+        public String solution(int n) {
+            return n % 2 == 0 ? "수박".repeat(n/2) : "수박".repeat(n/2) + "수";
+        }
+    }
+}
