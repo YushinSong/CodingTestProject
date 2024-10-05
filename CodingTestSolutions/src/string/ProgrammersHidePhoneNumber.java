529c4d708d926904a738858c3fa658d9fa1e9572
YushinSong <dbtls6513@naver.com>
2024-10-05 19:49:07 +0900

    [송유신] 프로그래머스 핸드폰 번호 가리기 문제

---
+++ CodingTestSolutions/src/string/ProgrammersHidePhoneNumber.java
@@ -0,0 +1,9 @@
+package src.string;
+
+public class ProgrammersHidePhoneNumber {
+    class Solution {
+        public String solution(String phone_number) {
+            return "*".repeat(phone_number.length()-4) + phone_number.substring(phone_number.length()-4);
+        }
+    }
+}
