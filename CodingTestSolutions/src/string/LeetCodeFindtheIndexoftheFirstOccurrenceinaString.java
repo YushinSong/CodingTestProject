3a71f63d9d15962c404e978f173246435b7b07f0
dbtls6513 <dbtls6513@naver.com>
2025-01-01 11:44:28 +0900

    [송유신] LeetCode Find the Index of the First Occurrence in a String 문제

---
+++ CodingTestSolutions/src/string/LeetCodeFindtheIndexoftheFirstOccurrenceinaString.java
@@ -0,0 +1,9 @@
+package src.string;
+
+public class LeetCodeFindtheIndexoftheFirstOccurrenceinaString {
+    class Solution {
+        public int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
+        }
+    }
+}
