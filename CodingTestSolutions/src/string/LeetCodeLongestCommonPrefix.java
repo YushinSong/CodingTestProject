2af839031bb95c5eb3ff5b7c30ceaba27f5fcaaf
dbtls6513 <dbtls6513@naver.com>
2025-01-02 23:21:30 +0900

    [송유신] LeetCode Longest Common Prefix 문제

---
+++ CodingTestSolutions/src/string/LeetCodeLongestCommonPrefix.java
@@ -0,0 +1,14 @@
+package src.string;
+
+public class LeetCodeLongestCommonPrefix {
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            String prefix = strs[0];
+            for(String s : strs)
+                while(s.indexOf(prefix) != 0)
+                    prefix = prefix.substring(0, prefix.length()-1);
+
+            return prefix;
+        }
+    }
+}
