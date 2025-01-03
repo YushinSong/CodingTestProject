2b202ef9337b6fb99658f34069126d29326408f6
dbtls6513 <dbtls6513@naver.com>
2025-01-03 15:38:24 +0900

    [송유신] LeetCode Length of Last Word 문제

---
+++ CodingTestSolutions/src/string/LeetCodeLengthofLastWord.java
@@ -0,0 +1,10 @@
+package src.string;
+
+public class LeetCodeLengthofLastWord {
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            String[] split = s.trim().split(" ");
+            return split[split.length-1].length();
+        }
+    }
+}
