74637f43d7172bc6a3b801294aaec04a0f7121e1
dbtls6513 <dbtls6513@naver.com>
2025-01-11 17:34:30 +0900

    [송유신] LeetCode Not Boring Movies 문제

---
+++ CodingTestSolutions/src/SQL/LeetCodeNotBoringMovies.java
@@ -0,0 +1,9 @@
+package src.SQL;
+
+public class LeetCodeNotBoringMovies {
+//    # Write your MySQL query statement below
+//    SELECT id, movie, description, rating
+//    FROM Cinema
+//    WHERE ID % 2 = 1 AND description != "boring"
+//    ORDER BY 4 DESC;
+}
