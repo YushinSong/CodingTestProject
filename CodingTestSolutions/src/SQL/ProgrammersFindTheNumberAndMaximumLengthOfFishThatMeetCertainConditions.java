cf43266587e0f52a8f7835d48efa5a05c2772748
dbtls6513 <dbtls6513@naver.com>
2025-01-25 15:31:28 +0900

    [송유신] 프로그래머스 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기 문제

---
+++ CodingTestSolutions/src/SQL/ProgrammersFindTheNumberAndMaximumLengthOfFishThatMeetCertainConditions.java
@@ -0,0 +1,10 @@
+package src.SQL;
+
+public class ProgrammersFindTheNumberAndMaximumLengthOfFishThatMeetCertainConditions {
+//    -- 코드를 작성해주세요
+//    SELECT COUNT(*) AS FISH_COUNT, MAX(IFNULL(LENGTH, 10)) AS MAX_LENGTH, FISH_TYPE
+//    FROM FISH_INFO
+//    GROUP BY FISH_TYPE
+//    HAVING AVG(IFNULL(LENGTH, 10)) >= 33
+//    ORDER BY FISH_TYPE;
+}
